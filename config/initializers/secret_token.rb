962dd300a997be65462606912d45fe8bf55fff03
Andrea Paraggio <andreaparaggio@gmail.com>
2011-11-24 23:01:27 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ilovewhite::Application.config.secret_token = 'dc616b890a3182b085c30f51ed6ccae738048c4f1bce8fc9c48dc63c055ab6024e4978bf4737f400b3c54033bc8b7ec38156ee46ea42dcac18f2ded22c513a33'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ilovewhite::Application.config.session_store :cookie_store, key: '_ilovewhite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ilovewhite::Application.config.session_store :active_record_store
