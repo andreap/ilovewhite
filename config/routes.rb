a75ba5a94ccf47db595a64b1d70440fdce4a57d1
Andrea Paraggio <andreaparaggio@gmail.com>
2011-11-26 01:58:26 +0100

    creato controller categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Ilovewhite::Application.routes.draw do
+  resources :categories
+
+  resources :home
+  root :to => 'home#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
